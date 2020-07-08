857996cc783c1c93ee5f535ddb7660dc623da707
Adithyavj <adithyavj@gmail.com>
2020-07-08 08:58:52 +0530

    Added Connection String in appsettings.json, added a model and a controller working still in progress..

---
+++ WebAPICRUDOps/Controllers/EmployeesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using WebAPICRUDOps.Data;
+using WebAPICRUDOps.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace WebAPICRUDOps.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        IConfiguration _iconfiguration;
+
+        public EmployeesController(IConfiguration iconfiguration)
+        {
+            _iconfiguration = iconfiguration;
+        }
+
+        public IActionResult Index()
+        {
+            // First way  
+            //string value1 = _iconfiguration.GetSection("Data").GetSection("DevConnection").Value;
+            // Second way  
+            string connection = _iconfiguration.GetValue<string>("Data:DevConnection");
+            return null;
+        }
+
+
+        [HttpGet]
+        [ActionName("GetEmployeeByID")]
+        public Employee Get(int id)
+        {
+            //return listEmp.First(e => e.ID == id);  
+            SqlDataReader reader = null;
+            SqlConnection myConnection = new SqlConnection();
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select * from tblEmployee where EmployeeId=" + id + "";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            Employee emp = null;
+            while (reader.Read())
+            {
+                emp = new Employee();
+                emp.EmployeeId = Convert.ToInt32(reader.GetValue(0));
+                emp.Name = reader.GetValue(1).ToString();
+            }
+            return emp;
+            myConnection.Close();
+        }
+    }
+}
---
+++ WebAPICRUDOps/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPICRUDOps.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; } = 0;
+
+        public string Name { get; set; } = "";
+
+        public string Gender { get; set; } = "";
+
+        public int Age { get; set; } = 0;
+        public string Position { get; set; } = "";
+
+        public int Salary { get; set; } = 0;
+    }
+}
